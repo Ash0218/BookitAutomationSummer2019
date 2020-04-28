ab56026381a182197c1faf446d03aa673e12520e
Ash0218 <ajykim218@gmail.com>
2020-04-28 13:59:49 -0400

    added properties files

--- src/test/java/com/bookit/runners/CucumberRunner.java
+++ src/test/java/com/bookit/runners/CucumberRunner.java
@@ -1,4 +1,16 @@
 package com.bookit.runners; // 020620
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test.resources/features",
+        glue = "com/bookit/step_definitions",
+        dryRun = true,
+        tags = "@api"
+        // run after this
+)
 public class CucumberRunner {
 }
